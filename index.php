0e403c10b6e130f4e0b475e359c48037dfff3b1d
kp2129 <kristapspolis0@gmail.com>
2023-11-03 13:45:16 +0200

    Izveidoju index.php ar html struktūru

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogs</title>
+</head>
+
+<body>
+    <main>
+        <div>
+            <div>
+                <h1></h1>
+            </div>
+            <div>
+                <p></p>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
